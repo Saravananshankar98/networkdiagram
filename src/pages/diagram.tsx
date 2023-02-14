cb48ec2a8f6b253464255053a043ad7baba126d3
Saravananshankar98 <saravananvijay005@gmail.com>
2023-02-14 18:16:31 +0530

    feat:create new project in diagram

---
+++ src/pages/diagram.tsx
@@ -0,0 +1,13 @@
+import {
+    DiagramComponent
+} from "@syncfusion/ej2-react-diagrams";
+
+function DiagramPage() {
+  return (
+    <div>
+      <DiagramComponent width={"600px"} height={"600px"}></DiagramComponent>
+    </div>
+  );
+}
+
+export default DiagramPage;
